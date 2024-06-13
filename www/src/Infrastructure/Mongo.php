a4783216d64852809318ce8d4a45384bbbb349e0
Eric Neves <ericnevesrodrigues@gmail.com>
2024-06-13 12:54:16 -0400

    Refactor: remove mongo

--- www/src/Infrastructure/Mongo.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Infrastructure;
-
-class Mongo
-{
-
-}
--- www/src/config/database.php
+++ www/src/config/database.php
@@ -1,18 +1,11 @@
 <?php
 
 return [
-    'pgsql'   => [
+    'pgsql' => [
         'host'     => $_ENV['PG_HOST'],
         'port'     => $_ENV['PG_PORT'],
         'database' => $_ENV['PG_DATABASE'],
         'username' => $_ENV['PG_USERNAME'],
         'password' => $_ENV['PG_PASSWORD'],
-    ],
-    'mongodb' => [
-        'host'     => $_ENV['MONGO_HOST'],
-        'port'     => $_ENV['MONGO_PORT'],
-        'database' => $_ENV['MONGO_DATABASE'],
-        'username' => $_ENV['MONGO_USERNAME'],
-        'password' => $_ENV['MONGO_PASSWORD'],
     ],
 ];
