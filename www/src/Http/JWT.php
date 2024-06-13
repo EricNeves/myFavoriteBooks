e44889bdf0a65368c263fede44057546fe6ca77a
Eric Neves <ericnevesrodrigues@gmail.com>
2024-06-12 22:20:13 -0400

    Feat: add jwt validation

--- www/src/Http/JWT.php
+++ www/src/Http/JWT.php
@@ -2,6 +2,8 @@
 
 namespace App\Http;
 
+use stdClass;
+
 class JWT
 {
     public function generateJWT(array $data = [])
@@ -17,6 +19,25 @@ class JWT
         return "$headerEncoded.$payloadEncoded.$signature";
     }
 
+    public function validateJWT(string $token): stdClass | bool
+    {
+        $parts = explode('.', $token);
+
+        if (count($parts) !== 3) {
+            return false;
+        }
+
+        [$headerEncoded, $payloadEncoded, $signature] = $parts;
+
+        if ($this->signature($headerEncoded, $payloadEncoded) !== $signature) {
+            return false;
+        }
+
+        $payload = $this->base64url_decode($payloadEncoded);
+
+        return $payload;
+    }
+
     protected function signature(string $header, string $payload): string
     {
         $signature = hash_hmac("sha256", "$header.$payload", $_ENV['JWT_SECRET'], true);
@@ -29,7 +50,7 @@ class JWT
         return rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
     }
 
-    protected function base64url_decode(string $data): string
+    protected function base64url_decode(string $data): stdClass
     {
         $padding = strlen($data) % 4;
 
@@ -37,6 +58,6 @@ class JWT
 
         $data = strtr($data, '-_', '+/');
 
-        return base64_decode($data);
+        return json_decode(base64_decode($data));
     }
 }
