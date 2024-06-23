1f97d4036ebdfeaf656e4ab52d07c84fba248378
Eric Neves <ericnevesrodrigues@gmail.com>
2024-06-23 16:44:23 -0400

    Feat: add and configure build

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 import { IEnvironment } from './environment.interface';
 
 export const environment: IEnvironment = {
-  baseUrl: 'http://localhost:4200',
+  baseUrl: 'http://localhost',
   api: {
     baseUrl: 'http://localhost:8000',
   },
